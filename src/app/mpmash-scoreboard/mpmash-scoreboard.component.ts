6125b2f343bdb23860fcb508bb6ba7f6a45463b1
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-24 20:16:53 +0100

    added ts and html to mp/mash/scores

--- src/app/mpmash-scoreboard/mpmash-scoreboard.component.ts
+++ src/app/mpmash-scoreboard/mpmash-scoreboard.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatTable } from '@angular/material';
+import { DataService } from '../data.service';
+
+export interface dataElement {
+  Name: string;
+  Rating: string;
+  Img: string;
+}  
+
+const ELEMENT_DATA: dataElement[] = [];
 
 @Component({
   selector: 'app-mpmash-scoreboard',
@@ -6,10 +17,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mpmash-scoreboard.component.scss']
 })
 export class MpmashScoreboardComponent implements OnInit {
+  public scoreboardForm: FormGroup; 
 
-  constructor() { }
+  genders = [
+    {gender_id: 0, gender: 'Male'},
+    {gender_id: 1, gender: 'Female'}
+  ];
+
+  displayedColumns: string[] = ['Image', 'Name', 'Cating'];
+  columnsToDisplay: string[] = this.displayedColumns;
+
+  constructor(private data: DataService) {}
+
+  dataScoreboard: dataElement[] = ELEMENT_DATA; 
+
+  @ViewChild(MatTable,{static:true}) table: MatTable<any>;
 
   ngOnInit() {
+    this.scoreboardForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(0), Validators.maxLength(60)]),
+      gender: new FormControl('', [Validators.required])
+    });
+
+    //this.dataScoreboard = this.data.fakeDatabase;
+    console.log(this.dataScoreboard);
+    console.log(this.data.fakeDatabase);
   }
+
+  filterScoreboard() { 
+
+    if (this.scoreboardForm.valid) {
+      
+      //filter update
+      
+      
+      this.table.renderRows(); 
+    }
+
+  } 
 
 }
