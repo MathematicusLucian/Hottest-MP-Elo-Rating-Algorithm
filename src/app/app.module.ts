456d8744187a28986eef25a12e2ddbeac60c74ed
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-24 13:10:41 +0100

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { MpProfileComponent } from './mp-profile/mp-profile.component';
 import { MpmashComponent } from './mpmash/mpmash.component';
 import { MpmashScoreboardComponent } from './mpmash-scoreboard/mpmash-scoreboard.component';
@@ -12,6 +13,7 @@ import { MpExpensesScoreboardComponent } from './mp-expenses-scoreboard/mp-expen
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     MpProfileComponent,
     MpmashComponent,
     MpmashScoreboardComponent,
