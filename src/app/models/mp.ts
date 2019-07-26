25e0f9b2422956cde55e927f03e520b074e8727d
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-26 22:24:06 +0100

    mp/profile component routing setup to capture id

---
+++ src/app/models/mp.ts
@@ -0,0 +1,8 @@
+export class MP {
+constructor(
+    id: string,
+    name: string,
+    gender: string,
+    rating: string,
+    img: string) {}
+}
--- src/app/mp-profile/mp-profile.component.ts
+++ src/app/mp-profile/mp-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MP } from "../models/mp";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mp-profile',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mp-profile.component.scss']
 })
 export class MpProfileComponent implements OnInit {
-
-  constructor() { }
+ 
+  mp: MP = "";
+  
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    console.log(this.route.snapshot);
+    this.mp = this.route.snapshot.paramMap.get('id');
+    console.log(this.mp);
   }
 
 }
