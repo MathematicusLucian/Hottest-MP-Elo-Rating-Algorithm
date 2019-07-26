b5535ec20d938a17c43c4d6c52d6785482857878
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-26 21:56:42 +0100

    moved ELO algorithm logic to back end

--- src/app/mpmash/mpmash.component.ts
+++ src/app/mpmash/mpmash.component.ts
@@ -23,13 +23,7 @@ export class MpmashComponent implements OnInit {
 
   total_mps = 0; 
 
-  twoMPsData = []; 
-
-  ea = 0;
-  eb = 0;
-  face_a_new_rating = 0;
-  face_b_new_rating = 0;
-  k = 24;  
+  twoMPsData = [];  
 
   success = '';
   error = '';
@@ -77,27 +71,5 @@ export class MpmashComponent implements OnInit {
         //this.error = err; 
       }
     ); 
-  }
-
-  //MOVE LOGIC TO BACK-END AND JUST PASS winnerID, loserID
-  calculateRatings(mp_chosen) {                 
-    let ea = 1/(1+10^((this.twoMPsData[0]["rating"] - this.twoMPsData[1]["rating"])/400));
-    let eb = 1/(1+10^((this.twoMPsData[1]["rating"] - this.twoMPsData[0]["rating"])/400));
-
-    let face_a_new_rating = 0;
-    let face_b_new_rating = 0;
-
-    if(mp_chosen == 0) {
-      face_a_new_rating = this.twoMPsData[0]["rating"] + (this.k * ea);
-      face_b_new_rating = this.twoMPsData[1]["rating"] - (this.k * eb);
-    } else {
-      face_a_new_rating = this.twoMPsData[0]["rating"] - (this.k * ea);
-      face_b_new_rating = this.twoMPsData[1]["rating"] + (this.k * eb);
-    }
-
-    face_a_new_rating = Math.round( face_a_new_rating * 1000 ) / 1000;
-    face_b_new_rating = Math.round( face_b_new_rating * 1000 ) / 1000;
-
-    return [face_a_new_rating, face_b_new_rating];
   }
 }
